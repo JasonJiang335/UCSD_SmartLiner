9cf3b4ad89e7def5ef63161acdfa090fd63e060e
JasonJiang335 <jiangzidong01@gmail.com>
2014-02-27 20:54:47 -0800

    change to push on heroku

--- config/database.js
+++
@@ -1,6 +0,0 @@
-// config/database.js
-module.exports = {
-
-	'url' : 'mongodb://<jiangzidong>:<Dong930122>@novus.modulusmongo.net:27017/yhawoM7a' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-
-};
